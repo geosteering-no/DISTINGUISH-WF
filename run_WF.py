8f5443cbe0b26ee1b98614e6def4eacf3afcfb55
Kristian Fossum <krfo@norceresearch.no>
2025-06-11 09:00:54 +0200

    structure and some initial code.. Note this is just copied from old, will not work

--- run_WF.py
+++ run_WF.py
@@ -1,8 +1,12 @@
-import numpy as np
+import subprocess
+import os
+import sys
 
 def main():
-    print("Hello World!")
+    app_path = os.path.join(os.path.dirname(__file__), "app.py")
+    # Launch Streamlit with the app
+    subprocess.run(["streamlit", "run", app_path] + sys.argv[1:])
 
-
-if __name__ == "__main__":
-    main()
+# USAGE
+# To run the Streamlit app, execute this script from the command line:
+# run_WF --server.port 8888
