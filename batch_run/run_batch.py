49e6082cca2c923dd6110deab0c9355c8d4fe981
Sergey Alyaev <cobxo3bot@gmail.com>
2025-10-27 15:17:03 +0100

    Fixed run_batch to run

--- batch_run/run_batch.py
+++ batch_run/run_batch.py
@@ -18,6 +18,7 @@ from pipt.loop.assimilation import Assimilate
 from pipt import pipt_init
 from input_output import read_config
 
+from wf_demo.default_load import input_dict, load_default_latent_tensor
 from wf_demo.write_data_var import new_data
 
 
@@ -26,31 +27,16 @@ global_extent = [0, 640, -16.25, 15.75]
 norm = Normalize(vmin=0.0, vmax=1)
 
 
-weights_folder = "https://gitlab.norceresearch.no/saly/image_to_log_weights/-/raw/master/em/{}.pth?ref_type=heads"
-scalers_folder = weights_folder
-full_em_model_file_name = "https://gitlab.norceresearch.no/saly/image_to_log_weights/-/raw/master/em/checkpoint_770.pth?ref_type=heads"
-# file_name = "https://gitlab.norceresearch.no/saly/image_to_log_weights/-/raw/master/gan/netG_epoch_15000.pth"
 
-
-def da(state, start_position):
+def da(state, start_position, input_dict):
     num_decissions = 1  # 64 # number of decissions to make
 
     # start_position = (32, 0) # initial position of the well
     # state = np.load('orig_prior.npz')['m'] # the prior latent vector
     np.savez('prior.npz', **{'x': state})  # save the prior as a file
 
-    kf = {'bit_pos': [start_position],
-          'vec_size': 60,
-          'reporttype': 'pos',
-          'reportpoint': [int(el) for el in range(1)],
-          'datatype': [('6kHz','83ft'),('12kHz','83ft'),('24kHz','83ft'),
-                       ('24kHz','43ft'),('48kHz','43ft'),('96kHz','43ft')],
-          'parallel': 1,
-          'file_name': file_name,
-          'full_em_model_file_name': full_em_model_file_name,
-         'scalers_folder': scalers_folder}
-
     kf = input_dict.copy()
+    kf['start_pos'] = [start_position]
 
     sim = GeoSim(kf)
 
@@ -92,5 +78,5 @@ if __name__ == "__main__":
         next_optimal = compute_and_apply_robot_suggestion(ensemble_state, start_position)
         path.append(next_optimal)
         start_position = next_optimal
-        ensemble_state = da(ensemble_state, next_optimal)
+        ensemble_state = da(ensemble_state, next_optimal, input_dict=input_dict)
 
