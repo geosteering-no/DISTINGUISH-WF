2225b0701bbf91d877aaa37ac9557f7072cb5917
Sergey Alyaev <cobxo3bot@gmail.com>
2025-11-11 16:30:19 +0100

    Comment out unused plotting code

--- batch_run/plot_result.py
+++ batch_run/plot_result.py
@@ -160,166 +160,166 @@ def plot_results_one_step(true_facies_image=None,
     ax_res.set_yticks(np.arange(-10, 11, 5))
     ax_res.yaxis.set_major_formatter(FuncFormatter(fmt_depth))
 
-    plt.show()
+    # plt.show()
 
-
-    # Load the decision points
-
-
-    gan_evaluator = full_nn_model.gan_evaluator
-
-
-
-    post_earth = np.array(
-        [create_weighted_image(evaluate_earth_model(gan_evaluator, state_vectors[:, el])) for el in
-         range(ne)])  # range(state.shape[1])])
-
-    # todo should we switch back to probability of sand ??? (use custom weights)
-    post_earth = np.array(
-        [create_weighted_image(evaluate_earth_model(gan_evaluator, state_vectors[:, el]), weights=[0., 1., 1.]) for el in
-         range(ne)])  # range(state.shape[1])])
-
-    next_optimal, _garbage_path = pathfinder().run(state_vectors, start_position_at_step)
-
-    # creating the figure
-    fig, ax = plt.subplots(figsize=(10, 5))
-
-    # visualizing the posterior
-    norm = Normalize(vmin=0.0, vmax=1)
-    im = ax.imshow(post_earth.mean(axis=0), cmap='tab20b', aspect='auto', norm=norm)
-
-
-    # visualizing the outline of the truth
-    x = np.array(range(64))
-    y = np.array(range(64))
-    X, Y = np.meshgrid(x, y)
-    Z = true_earth_model_facies[0, :, :]
-    contour_style = 'dashed'
-    contour_color = 'white'
-    contour = plt.contour(X, Y, Z, levels=0, colors=contour_color,
-                            linestyles=contour_style)
-    # contour.collections[0].set_label('Outline of sand+crevasse in the true model')
-
-    # visualizing the drilled path
-    path_rows, path_cols = zip(*(drilled_path))
-    # todo consider creating smooth path. Does not work easily when few points.
-    # # Creating a smooth curve
-    # x = np.array(path_cols)
-    # y = np.array(path_rows)
-    # x_smooth = np.linspace(x.min(), x.max(), 300)
-    # spl = make_interp_spline(x, y, k=3)  # k=3 for cubic spline
-    # y_smooth = spl(x_smooth)
-    ax.plot(path_cols, path_rows,
-            'k-', linewidth=3., label='Drilled path')
-    ax.plot(path_cols, path_rows,
-            'k*', label='Measurement locations')
-
-    if i == em_model_for_overlay_plotting_step:
-        ax_res.plot(path_cols, path_rows,
-                'k-', linewidth=3., label='Drilled path')
-        ax_res.plot(path_cols, path_rows,
-                'k*', label='Measurement locations')
-        # saving the true resistivity image with overlay
-        # Define the step-sizes
-        step_x = 10
-        step_y = 0.5
-
-        # Calculate the tick positions and labels
-        x_ticks_positions = np.arange(0, 64, 10)
-        x_ticks_labels = (x_ticks_positions - origin_x) * step_x
-
-        # Define the specific y-axis labels
-        y_ticks_labels = [-10, -5, 0, 5, 10]
-        y_ticks_labels_str = ['x090', 'x095', 'x100', 'x105', 'x110']
-        # Calculate the corresponding y tick positions
-        y_ticks_positions = [label / step_y + origin_y for label in y_ticks_labels]
-
-        # Set x-ticks and labels
-        ax_res.set_xticks(x_ticks_positions)
-        ax_res.set_xticklabels(x_ticks_labels)
-
-        # Set y-ticks and labels
-        ax_res.set_yticks(y_ticks_positions)
-        ax_res.set_yticklabels(y_ticks_labels_str)
-
-        fig_res.savefig(f'{plot_path}true_resistivity_{i}.png', bbox_inches='tight', dpi=600)
-        fig_res.savefig(f'{plot_path}true_resistivity_{i}.pdf', bbox_inches='tight')
-
-    if next_optimal[0] is not None:
-
-        # visualizing the next decision
-        path_rows = [start_position_at_step[0], next_optimal[0]]
-        path_cols = [start_position_at_step[1], next_optimal[1]]
-        ax.plot(path_cols, path_rows,
-                'k:', label='Proposed direction')
-
-        optimal_paths = [perform_dynamic_programming(post_earth[j, :, :], next_optimal,
-                                                     cost_vector=pathfinder().get_cost_vector())[2] for j in range(ne)]
-
-        # visualizing the optimal paths' remainders
-        earth_height = post_earth.shape[2]
-        max_height = earth_height - 0.5 - 0.01
-        min_height = 0.0 - 0.5 + 0.01
-        for j in range(ne):
-            path_rows, path_cols = zip(*(optimal_paths[j]))
-            noise_mult = 0.2
-            # noise_mult = 0
-            row_list = [el + noise_mult*np.random.randn() for el in path_rows]
-            row_list_truncated = [el if el < max_height else max_height for el in row_list]
-            row_list_truncated = [el if el > min_height else min_height for el in row_list_truncated]
-            if j == labelled_index:
-                ax.plot(path_cols, tuple(row_list_truncated),
-                        'k--', linewidth=0.25, label='Further trajectory options')
-            else:
-                # continue
-                ax.plot(path_cols, tuple(row_list_truncated),
-                        'k--', linewidth=0.25)
-        # ax.set_title('Result with Optimal Path', fontsize=18)
-        plt.tight_layout()
-
-
-        # Define the step-sizes
-        step_x = 10
-        step_y = 0.5
-
-        # Calculate the tick positions and labels
-        x_ticks_positions = np.arange(0, 64, 10)
-        x_ticks_labels = (x_ticks_positions - origin_x) * step_x
-
-        # Define the specific y-axis labels
-        y_ticks_labels =     [-10, -5, 0, 5, 10]
-        y_ticks_labels_str = ['x090', 'x095', 'x100', 'x105', 'x110']
-        # Calculate the corresponding y tick positions
-        y_ticks_positions = [label / step_y + origin_y for label in y_ticks_labels]
-
-        # Set x-ticks and labels
-        ax.set_xticks(x_ticks_positions)
-        ax.set_xticklabels(x_ticks_labels)
-        # Set y-ticks and labels
-        ax.set_yticks(y_ticks_positions)
-        ax.set_yticklabels(y_ticks_labels_str)
-
-        fig.savefig(f'{plot_path}mean_earth_{i}.png', bbox_inches='tight')
-        fig.savefig(f'{plot_path}mean_earth_{i}.pdf', bbox_inches='tight')
-
-        print(f'Saved step {i}')
-
-        if not saved_legend:
-            # Adding the legend outside the plot
-            # manually adding a line to the legend
-            cbar = plt.colorbar(im, ax=ax, location='bottom')
-            ax.plot([0], [0], linestyle=contour_style, color=contour_color,
-                     label='Outline of sand+crevasse in the true model')
-
-            legend = ax.legend(bbox_to_anchor=(1.25, 1), loc='upper left')
-            legend.get_frame().set_facecolor('lightgray')  # Set the background color to light gray
-            fig.savefig(f'{plot_path}legend.png', bbox_inches='tight')
-            fig.savefig(f'{plot_path}legend.pdf', bbox_inches='tight')
-            saved_legend = True
-
-        plt.close('all')
-        # drilled_path.append(checkpoint_at_step['pos'])# note that we compute another one during viosualization
-
-        # plt.show()
-
-        # plot the mean GAN output for the current decision points
+    #
+    # # Load the decision points
+    #
+    #
+    # gan_evaluator = full_nn_model.gan_evaluator
+    #
+    #
+    #
+    # post_earth = np.array(
+    #     [create_weighted_image(evaluate_earth_model(gan_evaluator, state_vectors[:, el])) for el in
+    #      range(ne)])  # range(state.shape[1])])
+    #
+    # # todo should we switch back to probability of sand ??? (use custom weights)
+    # post_earth = np.array(
+    #     [create_weighted_image(evaluate_earth_model(gan_evaluator, state_vectors[:, el]), weights=[0., 1., 1.]) for el in
+    #      range(ne)])  # range(state.shape[1])])
+    #
+    # next_optimal, _garbage_path = pathfinder().run(state_vectors, start_position_at_step)
+    #
+    # # creating the figure
+    # fig, ax = plt.subplots(figsize=(10, 5))
+    #
+    # # visualizing the posterior
+    # norm = Normalize(vmin=0.0, vmax=1)
+    # im = ax.imshow(post_earth.mean(axis=0), cmap='tab20b', aspect='auto', norm=norm)
+    #
+    #
+    # # visualizing the outline of the truth
+    # x = np.array(range(64))
+    # y = np.array(range(64))
+    # X, Y = np.meshgrid(x, y)
+    # Z = true_earth_model_facies[0, :, :]
+    # contour_style = 'dashed'
+    # contour_color = 'white'
+    # contour = plt.contour(X, Y, Z, levels=0, colors=contour_color,
+    #                         linestyles=contour_style)
+    # # contour.collections[0].set_label('Outline of sand+crevasse in the true model')
+    #
+    # # visualizing the drilled path
+    # path_rows, path_cols = zip(*(drilled_path))
+    # # todo consider creating smooth path. Does not work easily when few points.
+    # # # Creating a smooth curve
+    # # x = np.array(path_cols)
+    # # y = np.array(path_rows)
+    # # x_smooth = np.linspace(x.min(), x.max(), 300)
+    # # spl = make_interp_spline(x, y, k=3)  # k=3 for cubic spline
+    # # y_smooth = spl(x_smooth)
+    # ax.plot(path_cols, path_rows,
+    #         'k-', linewidth=3., label='Drilled path')
+    # ax.plot(path_cols, path_rows,
+    #         'k*', label='Measurement locations')
+    #
+    # if i == em_model_for_overlay_plotting_step:
+    #     ax_res.plot(path_cols, path_rows,
+    #             'k-', linewidth=3., label='Drilled path')
+    #     ax_res.plot(path_cols, path_rows,
+    #             'k*', label='Measurement locations')
+    #     # saving the true resistivity image with overlay
+    #     # Define the step-sizes
+    #     step_x = 10
+    #     step_y = 0.5
+    #
+    #     # Calculate the tick positions and labels
+    #     x_ticks_positions = np.arange(0, 64, 10)
+    #     x_ticks_labels = (x_ticks_positions - origin_x) * step_x
+    #
+    #     # Define the specific y-axis labels
+    #     y_ticks_labels = [-10, -5, 0, 5, 10]
+    #     y_ticks_labels_str = ['x090', 'x095', 'x100', 'x105', 'x110']
+    #     # Calculate the corresponding y tick positions
+    #     y_ticks_positions = [label / step_y + origin_y for label in y_ticks_labels]
+    #
+    #     # Set x-ticks and labels
+    #     ax_res.set_xticks(x_ticks_positions)
+    #     ax_res.set_xticklabels(x_ticks_labels)
+    #
+    #     # Set y-ticks and labels
+    #     ax_res.set_yticks(y_ticks_positions)
+    #     ax_res.set_yticklabels(y_ticks_labels_str)
+    #
+    #     fig_res.savefig(f'{plot_path}true_resistivity_{i}.png', bbox_inches='tight', dpi=600)
+    #     fig_res.savefig(f'{plot_path}true_resistivity_{i}.pdf', bbox_inches='tight')
+    #
+    # if next_optimal[0] is not None:
+    #
+    #     # visualizing the next decision
+    #     path_rows = [start_position_at_step[0], next_optimal[0]]
+    #     path_cols = [start_position_at_step[1], next_optimal[1]]
+    #     ax.plot(path_cols, path_rows,
+    #             'k:', label='Proposed direction')
+    #
+    #     optimal_paths = [perform_dynamic_programming(post_earth[j, :, :], next_optimal,
+    #                                                  cost_vector=pathfinder().get_cost_vector())[2] for j in range(ne)]
+    #
+    #     # visualizing the optimal paths' remainders
+    #     earth_height = post_earth.shape[2]
+    #     max_height = earth_height - 0.5 - 0.01
+    #     min_height = 0.0 - 0.5 + 0.01
+    #     for j in range(ne):
+    #         path_rows, path_cols = zip(*(optimal_paths[j]))
+    #         noise_mult = 0.2
+    #         # noise_mult = 0
+    #         row_list = [el + noise_mult*np.random.randn() for el in path_rows]
+    #         row_list_truncated = [el if el < max_height else max_height for el in row_list]
+    #         row_list_truncated = [el if el > min_height else min_height for el in row_list_truncated]
+    #         if j == labelled_index:
+    #             ax.plot(path_cols, tuple(row_list_truncated),
+    #                     'k--', linewidth=0.25, label='Further trajectory options')
+    #         else:
+    #             # continue
+    #             ax.plot(path_cols, tuple(row_list_truncated),
+    #                     'k--', linewidth=0.25)
+    #     # ax.set_title('Result with Optimal Path', fontsize=18)
+    #     plt.tight_layout()
+    #
+    #
+    #     # Define the step-sizes
+    #     step_x = 10
+    #     step_y = 0.5
+    #
+    #     # Calculate the tick positions and labels
+    #     x_ticks_positions = np.arange(0, 64, 10)
+    #     x_ticks_labels = (x_ticks_positions - origin_x) * step_x
+    #
+    #     # Define the specific y-axis labels
+    #     y_ticks_labels =     [-10, -5, 0, 5, 10]
+    #     y_ticks_labels_str = ['x090', 'x095', 'x100', 'x105', 'x110']
+    #     # Calculate the corresponding y tick positions
+    #     y_ticks_positions = [label / step_y + origin_y for label in y_ticks_labels]
+    #
+    #     # Set x-ticks and labels
+    #     ax.set_xticks(x_ticks_positions)
+    #     ax.set_xticklabels(x_ticks_labels)
+    #     # Set y-ticks and labels
+    #     ax.set_yticks(y_ticks_positions)
+    #     ax.set_yticklabels(y_ticks_labels_str)
+    #
+    #     fig.savefig(f'{plot_path}mean_earth_{i}.png', bbox_inches='tight')
+    #     fig.savefig(f'{plot_path}mean_earth_{i}.pdf', bbox_inches='tight')
+    #
+    #     print(f'Saved step {i}')
+    #
+    #     if not saved_legend:
+    #         # Adding the legend outside the plot
+    #         # manually adding a line to the legend
+    #         cbar = plt.colorbar(im, ax=ax, location='bottom')
+    #         ax.plot([0], [0], linestyle=contour_style, color=contour_color,
+    #                  label='Outline of sand+crevasse in the true model')
+    #
+    #         legend = ax.legend(bbox_to_anchor=(1.25, 1), loc='upper left')
+    #         legend.get_frame().set_facecolor('lightgray')  # Set the background color to light gray
+    #         fig.savefig(f'{plot_path}legend.png', bbox_inches='tight')
+    #         fig.savefig(f'{plot_path}legend.pdf', bbox_inches='tight')
+    #         saved_legend = True
+    #
+    #     plt.close('all')
+    #     # drilled_path.append(checkpoint_at_step['pos'])# note that we compute another one during viosualization
+    #
+    #     # plt.show()
+    #
+    #     # plot the mean GAN output for the current decision points
