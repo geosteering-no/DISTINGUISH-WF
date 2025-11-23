08ac16af1adeb16a9e432a469b67748bc7040421
Sergey Alyaev <cobxo3bot@gmail.com>
2025-11-23 21:33:55 +0100

    Update truth with latent vectors ++

--- data/write_data.py
+++ data/write_data.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 from GeoSim.sim import GeoSim
 
 import torch
@@ -8,7 +9,7 @@ from matplotlib.pyplot import GridSpec
 import pandas as pd
 import pickle
 import csv
-from wf_demo.default_load import input_dict, load_default_latent_tensor
+from wf_demo.default_load import input_dict, load_default_latent_tensor, save_realization_latent_tensor
 
 
 sim = GeoSim(input_dict)
@@ -25,133 +26,180 @@ sim.setup_fwd_run(**{'test':'foo'})
 # set_global_seed(55)  # fix seeds for reproducibility
 # set_global_seed(55)  # fix seeds for reproducibility
 
-my_latent_tensor = load_default_latent_tensor().to(device)
+for component in range(60, 60+1):
+    upper = 60
+    if component >= upper:
+        upper = component+2
+    for end_comp in range(component+1, upper, 7):
 
-index_vector = torch.full((1, 10), fill_value=32, dtype=torch.long).to(device)
-image, resistivity, logs = sim.NNmodel.forward(my_latent_tensor, index_vector, output_transien_results=True)
-
-# Get the last 8 values:
-#            ['USDA', 'USDP',
-#             'UADA', 'UADP',
-#             'UHRA', 'UHRP',
-#             'UHAA', 'UHAP'
-#         ]
-
-logs_np = logs.cpu().detach().numpy()[:,:,-8:]
-
-# the images are upside down, so we need to flip them
-# image = image.flip([2])
-# resistivity = resistivity.flip([2])
-# TODO flip is at least required for consistency of the Synth truth images
-
-# fig, (ax_img, ax_res) = plt.subplots(
-#     2, 1, figsize=(10, 8), sharex=True, height_ratios=[1, 2]
-# )
-#
-# # plotting the image
-# num_cols = image.shape[3]
-# # note, that the image is rotated weirdly in the current training
-# image_np = image.cpu().detach().numpy()
-# img = np.transpose(image_np[0, 0:3, :, :],(1, 2, 0))  # shape: [64, 64, 3]
-# ax_img.imshow(img, #extent=(-0.5, num_cols - 0.5, pad_top, pad_top + image.shape[2]),
-#               aspect='auto', interpolation='none')
-# ax_img.set_title("Facies image")
-# #ax_img.set_ylim(pad_top + image.shape[2], pad_top)
-#
-# # plotting resistivity
-# resistivity_np = resistivity.cpu().detach().numpy()
-# img_res = resistivity_np[:, 0, :].T  # shape: [H, W]
-# ax_res.imshow(img_res, extent=(-0.5, num_cols - 0.5, 0, resistivity.shape[2]),
-#               aspect='auto', interpolation='none', cmap='summer')
-# ax_res.set_title("Resistivity input")
-#
-# fig.savefig("Synth_Truth.png")
-
-names = [
-    # 'real(xx)', 'img(xx)',
-    # 'real(yy)', 'img(yy)',
-    # 'real(zz)', 'img(zz)',
-    # 'real(xz)', 'img(xz)',
-    # 'real(zx)', 'img(zx)',
-    'USDA', 'USDP',
-    'UADA', 'UADP',
-    'UHRA', 'UHRP',
-    'UHAA', 'UHAP'
-]
-tool_configs = [f'{f} khz - {s} ft' for f, s in zip([6., 12., 24., 24., 48., 96.], [83., 83., 83., 43., 43., 43.])]
-
-log_tool_index_in_use = 0
+        my_latent_tensor = load_default_latent_tensor().to(device)
+        my_size = my_latent_tensor.shape[1]
+        if component < my_size:
+            my_latent_tensor[:, component:end_comp] = -my_latent_tensor[:, component:end_comp]
+            save_realization_latent_tensor(my_latent_tensor, f"C1_minus_{component}_{end_comp}")
 
 
+        index_vector = torch.full((1, 40), fill_value=32, dtype=torch.long).to(device)
+        image, resistivity, logs = sim.NNmodel.forward(my_latent_tensor, index_vector, output_transien_results=True)
 
-fig = plt.figure(figsize=(10, 8))
+        # Get the last 8 values:
+        #            ['USDA', 'USDP',
+        #             'UADA', 'UADP',
+        #             'UHRA', 'UHRP',
+        #             'UHAA', 'UHAP'
+        #         ]
 
-# Create a grid with 3 rows and 2 columns
-# The second column is narrow for the colorbar
-gs = GridSpec(3, 2, width_ratios=[20, 1], height_ratios=[1, 2, 1.2], figure=fig)
+        logs_np = logs.cpu().detach().numpy()[:,:,-8:]
 
-# Left column: main plots
-ax_img = fig.add_subplot(gs[0, 0])
-ax_res = fig.add_subplot(gs[1, 0], sharex=ax_img)
-ax_logs = fig.add_subplot(gs[2, 0], sharex=ax_img)
+        # the images are upside down, so we need to flip them
+        # image = image.flip([2])
+        # resistivity = resistivity.flip([2])
+        # TODO flip is at least required for consistency of the Synth truth images
 
-# Right column: color-bars
-ax_img_cbar = fig.add_subplot(gs[0, 1])
-ax_res_cbar = fig.add_subplot(gs[1, 1])
-ax_logs_cbar = fig.add_subplot(gs[2, 1])
+        # fig, (ax_img, ax_res) = plt.subplots(
+        #     2, 1, figsize=(10, 8), sharex=True, height_ratios=[1, 2]
+        # )
+        #
+        # # plotting the image
+        # num_cols = image.shape[3]
+        # # note, that the image is rotated weirdly in the current training
+        # image_np = image.cpu().detach().numpy()
+        # img = np.transpose(image_np[0, 0:3, :, :],(1, 2, 0))  # shape: [64, 64, 3]
+        # ax_img.imshow(img, #extent=(-0.5, num_cols - 0.5, pad_top, pad_top + image.shape[2]),
+        #               aspect='auto', interpolation='none')
+        # ax_img.set_title("Facies image")
+        # #ax_img.set_ylim(pad_top + image.shape[2], pad_top)
+        #
+        # # plotting resistivity
+        # resistivity_np = resistivity.cpu().detach().numpy()
+        # img_res = resistivity_np[:, 0, :].T  # shape: [H, W]
+        # ax_res.imshow(img_res, extent=(-0.5, num_cols - 0.5, 0, resistivity.shape[2]),
+        #               aspect='auto', interpolation='none', cmap='summer')
+        # ax_res.set_title("Resistivity input")
+        #
+        # fig.savefig("Synth_Truth.png")
 
-pad_top = sim.NNmodel.pad_top
-# plotting the image
-num_cols = image.shape[3]
-# note, that the image is rotated weirdly in the current training
-img = image[0, 0:3, :, :].permute(1, 2, 0).cpu().numpy()  # shape: [64, 64, 3]
-ax_img.imshow(img,
-              extent=(-0.5, num_cols - 0.5, pad_top + image.shape[2], pad_top),
-              aspect='auto', interpolation='none')
-ax_img.set_title("Facies image")
-ax_img.set_ylim(pad_top + image.shape[2], pad_top)
+        names = [
+            # 'real(xx)', 'img(xx)',
+            # 'real(yy)', 'img(yy)',
+            # 'real(zz)', 'img(zz)',
+            # 'real(xz)', 'img(xz)',
+            # 'real(zx)', 'img(zx)',
+            'USDA', 'USDP',
+            'UADA', 'UADP',
+            'UHRA', 'UHRP',
+            'UHAA', 'UHAP'
+        ]
+        tool_configs = [f'{f} khz - {s} ft' for f, s in zip([6., 12., 24., 24., 48., 96.], [83., 83., 83., 43., 43., 43.])]
 
-num_cols_res = resistivity.shape[0]
-# plotting resistivity
-img_res = resistivity[:, 0, :].T.cpu().numpy()  # shape: [H, W]
-ax_res.imshow(img_res, extent=(-0.5, num_cols_res - 0.5, resistivity.shape[2], 0),
-              aspect='auto', interpolation='none', cmap='summer')
-ax_res.set_title("Resistivity input")
+        log_tool_index_in_use = 2
+        depth_in_use = 3
 
-# plotting logging locations
-mask = resistivity[:, 2, :].T.cpu().numpy()  # shape: [H, W]
-rgba = np.zeros((mask.shape[0], mask.shape[1], 4), dtype=np.float32)
-rgba[..., 0:3] = 0.0  # black color
-rgba[..., 3] = (mask != 0).astype(np.float32)  # alpha: 1 for non-zero, 0 for zero
 
-im_res = ax_res.imshow(rgba, extent=(-0.5, num_cols - 0.5, resistivity.shape[2], 0),
-              aspect='auto', interpolation='none')
-ax_res.set_ylim(resistivity.shape[2], 0)
-# ax_res.tick_params(labelbottom=True)
 
-# add colorbar
-cbar = fig.colorbar(im_res, cax=ax_res_cbar)
-cbar.set_label("Resistivity (normalized)")
+        fig = plt.figure(figsize=(10, 12))
 
-# Hide unused colorbar axes if desired
-ax_img_cbar.axis('off')
-ax_logs_cbar.axis('off')
+        # Create a grid with 3 rows and 2 columns
+        # The second column is narrow for the colorbar
+        gs = GridSpec(4, 2, width_ratios=[20, 1], height_ratios=[0.5, 1, 2, 1.2], figure=fig)
 
-#
-# plotting the logs
-ax_logs.set_title(names[log_tool_index_in_use])
-# logs_to_plot = logs_np[:, i]  # take the first batch and first channel
-for j, config in enumerate(tool_configs):
-    ax_logs.plot(logs_np[:, j, log_tool_index_in_use], label=config)
+        # Left column: main plots
+        ax = fig.add_subplot(gs[0, 0])
+        ax_img = fig.add_subplot(gs[1, 0])
+        ax_res = fig.add_subplot(gs[2, 0], sharex=ax_img)
+        ax_logs = fig.add_subplot(gs[3, 0], sharex=ax_img)
 
-ax_logs.legend()
+        # Right column: color-bars
+        ax_img_cbar = fig.add_subplot(gs[1, 1])
+        ax_res_cbar = fig.add_subplot(gs[2, 1])
+        ax_logs_cbar = fig.add_subplot(gs[3, 1])
 
-# remove visible labels on axes
-ax_img.tick_params(labelbottom=False)
-ax_res.tick_params(labelbottom=False)
+        # axes
+        ax.axhline(0)
+        ax.axvline(0)
 
-# saving
-fig.savefig(f'SynthTruth_updated.png', bbox_inches='tight', dpi=300)
+        v = my_latent_tensor[0].cpu().numpy()
+
+        v2 = v[2:]
+
+        # main 2D arrow (first two components)
+        ax.arrow(0, 0, v2[0], v2[1],
+                 head_width=abs(v2[1] / 10.), length_includes_head=True)
+
+        # label at head
+        ax.text(v2[0], v2[1],
+                f"(..., {v2[0]:.2f}, {v2[1]:.2f}, {v2[2]:.2f}, {v2[3]:.2f}, {v2[4]:.2f}, ...)",
+                ha="left", va="bottom")
+
+        ax.set_aspect("equal") # , "box")
+        ax.set_title("Latent vector")
+        ax.axis("off")
+
+        pad_top = sim.NNmodel.pad_top
+        # plotting the image
+        num_cols = image.shape[3]
+        # note, that the image is rotated weirdly in the current training
+        img = image[0, 0:3, :, :].permute(1, 2, 0).cpu().numpy()  # shape: [64, 64, 3]
+        ax_img.imshow(img,
+                      extent=(-0.5, num_cols - 0.5, pad_top + image.shape[2], pad_top),
+                      aspect='auto', interpolation='none')
+        ax_img.set_title("Facies image")
+        ax_img.set_ylim(pad_top + image.shape[2], pad_top)
+
+        num_cols_res = resistivity.shape[0]
+        # plotting resistivity
+        img_res = resistivity[:, 0, :].T.cpu().numpy()  # shape: [H, W]
+        im_res_1 = ax_res.imshow(img_res, extent=(-0.5, num_cols_res - 0.5, resistivity.shape[2], 0),
+                      aspect='auto', interpolation='none', cmap='summer')
+        ax_res.set_title("Resistivity padded input (h-component)")
+
+        # plotting logging locations
+        mask = resistivity[:, 2, :].T.cpu().numpy()  # shape: [H, W]
+        rgba = np.zeros((mask.shape[0], mask.shape[1], 4), dtype=np.float32)
+        rgba[..., 0:3] = 0.0  # black color
+        rgba[..., 3] = (mask != 0).astype(np.float32)  # alpha: 1 for non-zero, 0 for zero
+
+        im_res = ax_res.imshow(rgba, extent=(-0.5, num_cols - 0.5, resistivity.shape[2], 0),
+                      aspect='auto', interpolation='none')
+        ax_res.set_ylim(resistivity.shape[2], 0)
+        # ax_res.tick_params(labelbottom=True)
+
+        # add colorbar
+        cbar = fig.colorbar(im_res_1, cax=ax_res_cbar)
+        cbar.set_label("Resistivity (Ohm m)")
+        # TODO there is some bug with the color scale when there is no sand
+
+        # Hide unused colorbar axes if desired
+        ax_img_cbar.axis('off')
+        ax_logs_cbar.axis('off')
+
+        #
+        # plotting the logs
+        # ax_logs.set_title(names[log_tool_index_in_use])
+        # # logs_to_plot = logs_np[:, i]  # take the first batch and first channel
+        # for j, config in enumerate(tool_configs):
+        #     ax_logs.plot(logs_np[:, j, log_tool_index_in_use], label=config)
+
+        ax_logs.set_title(str(tool_configs[depth_in_use]))
+        for j, config in enumerate(names):
+            ax_logs.plot(logs_np[:, depth_in_use, j], label=config)
+
+
+        ax_logs.legend()
+
+        # remove visible labels on axes
+        ax_img.tick_params(labelbottom=False)
+        ax_res.tick_params(labelbottom=False)
+
+        # saving
+        if component < my_size:
+            fig.savefig(f'SynthTruth_updated_{component}_{end_comp}.png', bbox_inches='tight', dpi=300)
+        else:
+            fig.savefig(f'SynthTruth_updated.png', bbox_inches='tight', dpi=300)
+
+        plt.close(fig)
+
+
 
 k = open('assim_index.csv','w',newline='')
 #writer4 = csv.writer(k)
